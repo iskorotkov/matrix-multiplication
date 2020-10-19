13425af4f624560ce98e08e8a2ffb30ce6ad23d6
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-19 13:08:49 +0500

    refactor(naive): merge test classes for naive algorithm together

--- tests/NaiveMultiplication.Tests/LargeSquareMatrices.cs
+++ tests/NaiveMultiplication.Tests/LargeSquareMatrices.cs
@@ -12,9 +12,17 @@ namespace NaiveMultiplication.Tests
         private readonly MatrixGenerator _generator = new MatrixGenerator(-100, 100);
 
         [Theory, InlineData(5), InlineData(10), InlineData(20)]
-        public void MultiplyMatrices(int size)
+        public void MultiplySquareMatrices(int size)
         {
             var (a, b) = _generator.NewMatrixPair(size, size, size);
+            var expected = DenseMatrix.OfArray(a) * DenseMatrix.OfArray(b);
+            _solver.Multiply(a, b).ShouldBe(expected.ToArray());
+        }
+
+        [Theory, InlineData(5, 10, 15), InlineData(10, 15, 10), InlineData(20, 25, 15)]
+        public void MultiplyRectangularMatrices(int aRows, int aColumns, int bColumns)
+        {
+            var (a, b) = _generator.NewMatrixPair(aRows, aColumns, bColumns);
             var expected = DenseMatrix.OfArray(a) * DenseMatrix.OfArray(b);
             _solver.Multiply(a, b).ShouldBe(expected.ToArray());
         }
