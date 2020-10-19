1dd9c0e855c726f624552abd8538e8a8a3295782
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-19 10:56:59 +0500

    fix(matrix): change base class of MatrixAccessException

--- src/MatrixTypes/MatrixAccessException.cs
+++ src/MatrixTypes/MatrixAccessException.cs
@@ -2,10 +2,9 @@
 
 namespace MatrixTypes
 {
-    public class MatrixAccessException : ArgumentException
+    public class MatrixAccessException : ArgumentOutOfRangeException
     {
-        public MatrixAccessException(string paramName) : base("Value should be in a valid range of matrix view.",
-            paramName)
+        public MatrixAccessException(string paramName) : base(paramName, "Value should be in a valid range of matrix view.")
         {
         }
     }
