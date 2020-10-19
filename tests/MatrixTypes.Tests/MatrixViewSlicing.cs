b71c2cf73fc54c23a6f55ca44bb5bf562278618a
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-19 14:23:50 +0500

    test(matrix): add tests for Slice method of Range and MatrixView types

---
+++ tests/MatrixTypes.Tests/MatrixViewSlicing.cs
@@ -0,0 +1,52 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace MatrixTypes.Tests
+{
+    public class MatrixViewSlicing
+    {
+        private readonly MatrixView _view = new MatrixView(new double[,]
+        {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        });
+
+        [Fact]
+        public void FullSlice()
+        {
+            _view.Slice(new Range(0, 4), new Range(0, 4)).ShouldBe(_view);
+        }
+
+        [Theory, InlineData(0, 3, 0, 3), InlineData(2, 4, 2, 4), InlineData(3, 4, 0, 3), InlineData(2, 4, 1, 4)]
+        public void ShrinkingSlice(int rowsStart, int rowsEnd, int columnsStart, int columnsEnd)
+        {
+            var rows = new Range(rowsStart, rowsEnd);
+            var columns = new Range(columnsStart, columnsEnd);
+
+            var slice = _view.Slice(rows, columns);
+            EnsureEqual(slice, rows, columns);
+        }
+
+        [Fact]
+        public void SeveralSlicesInRow()
+        {
+            var s1 = _view.Slice(new Range(1, 4), new Range(0, 3));
+            var s2 = s1.Slice(new Range(0, 2), new Range(1, 3));
+            var s3 = s2.Slice(new Range(1, 2), new Range(0, 1));
+            s3[0, 0].ShouldBe(_view[2, 1]);
+        }
+
+        private void EnsureEqual(MatrixView view, Range rows, Range columns)
+        {
+            for (var i = 0; i < rows.Count; i++)
+            {
+                for (var j = 0; j < columns.Count; j++)
+                {
+                    view[i, j].ShouldBe(_view[rows.Start + i, columns.Start + j]);
+                }
+            }
+        }
+    }
+}
---
+++ tests/MatrixTypes.Tests/RangeSlicing.cs
@@ -0,0 +1,36 @@
+using Shouldly;
+using Xunit;
+
+namespace MatrixTypes.Tests
+{
+    public class RangeSlicing
+    {
+        private readonly Range _r = new Range(0, 10);
+
+        [Fact]
+        public void FromStart()
+        {
+            _r.Slice(new Range(8, 10)).ShouldBe(new Range(8, 10));
+        }
+
+        [Fact]
+        public void FromEnd()
+        {
+            _r.Slice(new Range(0, 9)).ShouldBe(new Range(0, 9));
+        }
+
+        [Fact]
+        public void FromBothSides()
+        {
+            _r.Slice(new Range(3, 8)).ShouldBe(new Range(3, 8));
+        }
+
+        [Fact]
+        public void EmptyRange()
+        {
+            _r.Slice(new Range(5, 2)).ShouldBe(new Range(5, 2));
+            _r.Slice(new Range(0, 0)).ShouldBe(new Range(0, 0));
+            _r.Slice(new Range(9, 9)).ShouldBe(new Range(9, 9));
+        }
+    }
+}
