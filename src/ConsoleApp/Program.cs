5587ad1b7300e787edf324acfaafc142d7b7023b
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-26 13:05:20 +0500

    feat(console): add console app

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MatrixParser;
+using MatrixTypes;
+
+namespace ConsoleApp
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var parser = new Parser();
+            var (a, b, _) = parser.Parse(Console.In);
+
+            Console.WriteLine("A =");
+            Console.WriteLine(new MatrixView(a));
+            Console.WriteLine("B =");
+            Console.WriteLine(new MatrixView(b));
+        }
+    }
+}
