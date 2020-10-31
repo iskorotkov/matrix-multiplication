42607f64a7e18e0aad65f3212668917aa7a5ec4d
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-31 11:13:50 +0500

    wip

--- src/StrassenAlgorithm/StrassenSolver.cs
+++ src/StrassenAlgorithm/StrassenSolver.cs
@@ -3,6 +3,29 @@ using MatrixTypes;
 
 namespace StrassenAlgorithm
 {
+    public readonly struct MatrixView
+    {
+        public readonly double[,] Values;
+        public readonly int StartRow;
+        public readonly int Rows;
+        public readonly int StartColumn;
+        public readonly int Columns;
+        public readonly int RealRows;
+        public readonly int RealColumns;
+
+        public MatrixView(double[,] values, int startRow, int startColumn, int rows, int columns, int realRows,
+            int realColumns)
+        {
+            Values = values;
+            StartRow = startRow;
+            Rows = rows;
+            StartColumn = startColumn;
+            Columns = columns;
+            RealRows = realRows;
+            RealColumns = realColumns;
+        }
+    }
+
     public class StrassenSolver : ISolver
     {
         public double[,] Multiply(double[,] a, double[,] b)
@@ -15,46 +38,57 @@ namespace StrassenAlgorithm
             );
             var desiredDimension = CalculateDesiredDimension(dimension);
 
+            MatrixView viewA, viewB;
             if (a.GetLength(0) != desiredDimension || a.GetLength(1) != desiredDimension)
             {
-                a = Extend(a, desiredDimension, desiredDimension);
+                viewA = new MatrixView(a, 0, 0, desiredDimension, desiredDimension, a.GetLength(0), a.GetLength(1));
+            }
+            else
+            {
+                viewA = new MatrixView(a, 0, 0, desiredDimension, desiredDimension, desiredDimension, desiredDimension);
             }
 
             if (b.GetLength(0) != desiredDimension || b.GetLength(1) != desiredDimension)
             {
-                b = Extend(b, desiredDimension, desiredDimension);
+                viewB = new MatrixView(b, 0, 0, desiredDimension, desiredDimension, b.GetLength(0), b.GetLength(1));
+            }
+            else
+            {
+                viewB = new MatrixView(b, 0, 0, desiredDimension, desiredDimension, desiredDimension, desiredDimension);
             }
 
-            var result = MultiplyRecursive(a, b);
-            return Shrink(result, rows, cols, 0, 0);
+            var result = MultiplyRecursive(viewA, viewB);
+            var shrinked = new MatrixView(result, 0, 0, rows, cols, rows, cols);
+            return ToArray(shrinked);
         }
 
-        private static double[,] MultiplyRecursive(double[,] a, double[,] b)
+        private static double[,] MultiplyRecursive(MatrixView a, MatrixView b)
         {
             // a and b are always square matrices of the same dimension => check if contain single cell
-            if (a.GetLength(0) == 1)
+            if (a.Rows == 1)
             {
-                return new[,] {{a[0, 0] * b[0, 0]}};
+                return new[,] {{a.Values[a.StartRow, a.StartColumn] * b.Values[b.StartRow, b.StartColumn]}};
             }
 
             // a and b are always square matrices of the same dimension => can skip taking max dimension and use number of rows
-            var actualDimension = a.GetLength(0);
+            var actualDimension = a.Rows;
             var desiredDimension = CalculateDesiredDimension(actualDimension);
             if (actualDimension != desiredDimension)
             {
-                (a, b) = (Extend(a, desiredDimension, desiredDimension), Extend(b, desiredDimension, desiredDimension));
+                (a, b) = (Extend(a, desiredDimension, desiredDimension),
+                    Extend(b, desiredDimension, desiredDimension));
             }
 
             var (a11, a12, a21, a22) = SubdivideSquareMatrix(a);
             var (b11, b12, b21, b22) = SubdivideSquareMatrix(b);
 
-            var m1 = MultiplyRecursive(a12.Subtract(a22), b21.Add(b22));
-            var m2 = MultiplyRecursive(a11.Add(a22), b11.Add(b22));
-            var m3 = MultiplyRecursive(a11.Subtract(a21), b11.Add(b12));
-            var m4 = MultiplyRecursive(a11.Add(a12), b22);
-            var m5 = MultiplyRecursive(a11, b12.Subtract(b22));
-            var m6 = MultiplyRecursive(a22, b21.Subtract(b11));
-            var m7 = MultiplyRecursive(a21.Add(a22), b11);
+            var m1 = MultiplyRecursive(Subtract(a12, a22), Add(b21, b22));
+            var m2 = MultiplyRecursive(Add(a11, a22), Add(b11, b22));
+            var m3 = MultiplyRecursive(Subtract(a11, a21), Add(b11, b12));
+            var m4 = MultiplyRecursive(Add(a11, a12), b22);
+            var m5 = MultiplyRecursive(a11, Subtract(b12, b22));
+            var m6 = MultiplyRecursive(a22, Subtract(b21, b11));
+            var m7 = MultiplyRecursive(Add(a21, a22), b11);
 
             var c = new double[desiredDimension, desiredDimension];
             var halfDimension = desiredDimension / 2;
@@ -73,52 +107,92 @@ namespace StrassenAlgorithm
             return c;
         }
 
-        private static double[,] Extend(double[,] x, int rows, int columns)
+        private static double[,] ToArray(MatrixView x)
         {
-            var result = new double[rows, columns];
-            for (var i = 0; i < x.GetLength(0); i++)
+            var result = new double[x.Rows, x.Columns];
+            for (var i = 0; i < x.Rows; i++)
             {
-                for (var j = 0; j < x.GetLength(1); j++)
+                for (var j = 0; j < x.Columns; j++)
                 {
-                    result[i, j] = x[i, j];
-                }
-
-                for (var j = x.GetLength(1); j < columns; j++)
-                {
-                    result[i, j] = 0d;
-                }
-            }
-
-            for (var i = x.GetLength(0); i < rows; i++)
-            {
-                for (var j = 0; j < columns; j++)
-                {
-                    result[i, j] = 0d;
+                    result[i, j] = x.Values[x.StartRow + i, x.StartColumn + j];
                 }
             }
 
             return result;
         }
 
-        private static double[,] Shrink(double[,] x, int rows, int columns, int rowsOffset, int columnsOffset)
+        private static MatrixView Extend(MatrixView x, int rows, int columns) =>
+            new MatrixView(x.Values, 0, 0, rows, columns, x.RealRows, x.RealColumns);
+
+        private static MatrixView Add(MatrixView a, MatrixView b)
         {
-            var result = new double[rows, columns];
-            for (var i = 0; i < rows; i++)
+            var result = new double[a.Rows, b.Columns];
+            var (realRows, realColumns) = (Math.Min(a.RealRows, b.RealRows), Math.Min(a.RealColumns, b.RealColumns));
+            for (var i = 0; i < realRows; i++)
             {
-                for (var j = 0; j < columns; j++)
+                for (var j = 0; j < realColumns; j++)
                 {
-                    result[i, j] = x[rowsOffset + i, columnsOffset + j];
+                    result[i, j] = a.Values[a.StartRow + i, a.StartColumn + j] +
+                                   b.Values[b.StartRow + i, b.StartColumn + j];
+                }
+
+                for (var j = realColumns; j < b.Columns; j++)
+                {
+                    result[i, j] = 0d;
                 }
             }
 
-            return result;
+            for (var i = realRows; i < a.Rows; i++)
+            {
+                for (var j = 0; j < b.Columns; j++)
+                {
+                    result[i, j] = 0d;
+                }
+            }
+
+            return new MatrixView(result, 0, 0, a.Rows, b.Columns, a.Rows, b.Columns);
         }
+
+        private static MatrixView Subtract(MatrixView a, MatrixView b)
+        {
+            var result = new double[a.Rows, b.Columns];
+            var (realRows, realColumns) = (Math.Min(a.RealRows, b.RealRows), Math.Min(a.RealColumns, b.RealColumns));
+            for (var i = 0; i < realRows; i++)
+            {
+                for (var j = 0; j < realColumns; j++)
+                {
+                    result[i, j] = a.Values[a.StartRow + i, a.StartColumn + j] -
+                                   b.Values[b.StartRow + i, b.StartColumn + j];
+                }
+
+                for (var j = realColumns; j < b.Columns; j++)
+                {
+                    result[i, j] = 0d;
+                }
+            }
+
+            for (var i = realRows; i < a.Rows; i++)
+            {
+                for (var j = 0; j < b.Columns; j++)
+                {
+                    result[i, j] = 0d;
+                }
+            }
+
+            return new MatrixView(result, 0, 0, a.Rows, b.Columns, a.Rows, b.Columns);
+        }
+
+        private static MatrixView Shrink(MatrixView x, int rows, int columns, int rowsOffset, int columnsOffset) =>
+            new MatrixView(x.Values, rowsOffset, columnsOffset, rows, columns,
+                Math.Min(Math.Abs(x.RealRows - rowsOffset), rows),
+                Math.Min(Math.Abs(x.RealColumns - columnsOffset), columns));
 
         private static int CalculateDesiredDimension(int dimension) => dimension % 2 == 0 ? dimension : dimension + 1;
 
-        private static (double[,] X11, double[,] X12, double[,] X21, double[,] X22) SubdivideSquareMatrix(double[,] x)
+        private static (MatrixView X11, MatrixView X12, MatrixView X21, MatrixView X22) SubdivideSquareMatrix(
+            MatrixView x)
         {
-            var halfSize = x.GetLength(0) / 2;
+            var halfSize = x.Rows / 2;
             return (
                 Shrink(x, halfSize, halfSize, 0, 0),
                 Shrink(x, halfSize, halfSize, 0, halfSize),
